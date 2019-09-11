89ee593aabad3f353da36f476a25da8eb967a9f0
a <a@163.com>
2019-09-11 14:34:17 +0800

    first add

---
+++ src/main/java/com/woniuxy/oa/Springboot08GitApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot08GitApplication.class, args);
+	}
+
+}
